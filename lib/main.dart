967a045b508fd396ea2106ce11ed8e8bf1fa5088
Muhammed Alaa Eddin Mustafa <dodo1003pro@gmail.com>
2024-06-10 18:24:31 +0300

    added basket ball application

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+import 'package:basketball_points_counter/basketball_counter.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: pointsCounter(),
+  ));
+}
+
